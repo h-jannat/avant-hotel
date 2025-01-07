5610eaccf6a0b69ab5440dcf7a1390f9bf52e1e3
h-jannat <h.jannat@lamah.com>
2025-01-07 19:47:45 +0200

    create new components

---
+++ frontend/src/app/guest-details/guest-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-details',
+  imports: [],
+  templateUrl: './guest-details.component.html',
+  styleUrl: './guest-details.component.less'
+})
+export class GuestDetailsComponent {
+
+}
---
+++ frontend/src/app/room-details/room-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-details',
+  imports: [],
+  templateUrl: './room-details.component.html',
+  styleUrl: './room-details.component.less'
+})
+export class RoomDetailsComponent {
+
+}
