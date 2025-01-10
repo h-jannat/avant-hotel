9f291ff316f88c4e72f30b98709fcfcf222ff0a2
h-jannat <h.jannat@lamah.com>
2025-01-10 05:15:30 +0200

    remove send, rename pagination param

--- backend/src/api/rooms/roomController.ts
+++ backend/src/api/rooms/roomController.ts
@@ -6,33 +6,33 @@ import { logger } from "@/app";
 class RoomController {
   public getAllRooms: RequestHandler = async (req: Request, res: Response) => {
     const page = parseInt(req.query.page as string, 10) || 1;
-    const perPage = parseInt(req.query.perPage as string, 10) || 10;
-    const offset = (page - 1) * perPage;
+    const limit = parseInt(req.query.limit as string, 10) || 10;
+    const offset = (page - 1) * limit;
     const { sortBy, sortDirection } = req.query;
 
     const rooms = await roomService.findAll({
       offset,
-      limit: perPage,
+      limit: limit,
       sortBy: sortBy as string | undefined,
       sortDirection: sortDirection as string | undefined,
     });
-    res.json(rooms);
+    res.json({ ...rooms, limit, page });
   };
 
   public getRoom: RequestHandler = async (req: Request, res: Response) => {
     const room = await roomService.findById(req.params.id);
-    res.json(room).send();
+    res.json(room);
   };
 
   public createRoom: RequestHandler = async (req: Request, res: Response) => {
-    await roomService.create(req.body);
-    res.json({ message: "room is created" }).send();
+    const result = await roomService.create(req.body);
+    res.json(result);
   };
 
   public updateRoom: RequestHandler = async (req: Request, res: Response) => {
     logger.debug(`id = ${req.params.id}`);
     const result = await roomService.update(req.params.id, req.body);
-    res.json(result).send();
+    res.json(result);
   };
 }
 
