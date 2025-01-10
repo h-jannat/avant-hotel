e9a2284fe49695c31279cf1c125066200675440c
h-jannat <h.jannat@lamah.com>
2025-01-10 05:22:51 +0200

    add more routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
-import { RoomsComponent } from './rooms/rooms.component';
-import { GuestsComponent } from './guests/guests.component';
-import { ReservationsComponent } from './reservations/reservations.component';
-import { CreateRoomComponent } from './create-room/create-room.component';
 import { CreateGuestComponent } from './create-guest/create-guest.component';
 import { CreateReservationComponent } from './create-reservation/create-reservation.component';
-import { EditRoomComponent } from './edit-room/edit-room.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
 import { EditGuestComponent } from './edit-guest/edit-guest.component';
 import { EditReservationComponent } from './edit-reservation/edit-reservation.component';
-import { RoomDetailsComponent } from './room-details/room-details.component';
+import { EditRoomComponent } from './edit-room/edit-room.component';
 import { GuestDetailsComponent } from './guest-details/guest-details.component';
+import { GuestsComponent } from './guests/guests.component';
 import { ReservationDetailsComponent } from './reservation-details/reservation-details.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 export const roomsPath: string = 'rooms';
 export const guestsPath: string = 'guests';
@@ -24,14 +24,12 @@ export const routes: Routes = [
   {
     path: roomsPath,
     component: RoomsComponent,
-    children: [
-      { path: createPath, component: CreateRoomComponent },
-      {
-        path: detailsPath,
-        component: RoomDetailsComponent,
-        children: [{ path: editPath, component: EditRoomComponent }],
-      },
-    ],
+    children: [{ path: createPath, component: CreateRoomComponent }],
+  },
+  {
+    path: `${roomsPath}/${detailsPath}`,
+    component: RoomDetailsComponent,
+    children: [{ path: editPath, component: EditRoomComponent }],
   },
   {
     path: guestsPath,
