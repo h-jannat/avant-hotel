46471a20aeb89f557448e42721815df8dcff7d4b
h-jannat <h.jannat@lamah.com>
2025-01-10 22:39:33 +0200

    convert to int

--- backend/src/common/middleware/rateLimiter.ts
+++ backend/src/common/middleware/rateLimiter.ts
@@ -3,10 +3,12 @@ import { rateLimit } from "express-rate-limit";
 
 const rateLimiter = rateLimit({
   legacyHeaders: true,
-  limit: process.env.COMMON_RATE_LIMIT_MAX_REQUESTS,
+  limit: parseInt(process.env.COMMON_RATE_LIMIT_MAX_REQUESTS || "1000"),
   message: "Too many requests, please try again later.",
   standardHeaders: true,
-  windowMs: process.env.COMMON_RATE_LIMIT_WINDOW_MS,
+  windowMs: parseInt(
+    process.env.COMMON_RATE_LIMIT_WINDOW_MS || `${15 * 60 * 1000}`
+  ),
   keyGenerator: (req: Request) => req.ip as string,
 });
 
