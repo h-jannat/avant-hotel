5fce6e232bd3e9ac5a171d958e051beeb5ea19e7
h-jannat <h.jannat@lamah.com>
2025-01-10 05:25:39 +0200

    create edit room form

--- frontend/src/app/edit-room/edit-room.component.ts
+++ frontend/src/app/edit-room/edit-room.component.ts
@@ -1,11 +1,93 @@
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  TuiAppearance,
+  TuiButton,
+  TuiError,
+  TuiIcon,
+  TuiNotification,
+  TuiTextfield,
+  TuiTitle,
+} from '@taiga-ui/core';
+import {
+  TuiFieldErrorPipe,
+  TuiSegmented,
+  TuiSwitch,
+  TuiTooltip,
+} from '@taiga-ui/kit';
+import { TuiCardLarge, TuiForm, TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-edit-room',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    NgIf,
+    ReactiveFormsModule,
+    TuiAppearance,
+    TuiButton,
+    TuiCardLarge,
+    TuiError,
+    TuiFieldErrorPipe,
+    TuiForm,
+    TuiHeader,
+    TuiIcon,
+    TuiNotification,
+    TuiSegmented,
+    TuiSwitch,
+    TuiTextfield,
+    TuiTitle,
+    TuiTooltip,
+  ],
   templateUrl: './edit-room.component.html',
-  styleUrl: './edit-room.component.less'
+  styleUrl: './edit-room.component.less',
 })
 export class EditRoomComponent {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) // private roomService: RoomService
+  {}
 
+  update() {
+    throw new Error('Method not implemented.');
+  }
+
+  cancel() {
+    this.router.navigate(['/rooms']);
+  }
+
+  protected readonly form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    number: new FormControl('', Validators.required),
+  });
+
+  ngOnInit() {
+    console.log(this.route.toString());
+    // let roomId = this.route.snapshot.paramMap.get('id');
+    // console.log(roomId);
+
+    // if (!roomId) {
+    //   this.router.navigate(['../../'], { relativeTo: this.route });
+    //   return;
+    // }
+
+    // this.roomService.getRoomById(roomId).subscribe((room) => {
+    //   if (!room) {
+    //     this.router.navigate(['../../'], { relativeTo: this.route });
+    //     return;
+    //   }
+
+    //   this.form.patchValue({
+    //     name: (room as any).name,
+    //     number: (room as any).number,
+    //   });
+    // });
+  }
 }
