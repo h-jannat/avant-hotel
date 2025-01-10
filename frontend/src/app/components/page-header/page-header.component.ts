240cf912150cb3bccec1795a81f90737e0b210b5
h-jannat <h.jannat@lamah.com>
2025-01-10 05:25:12 +0200

    add on create and on click methods

--- frontend/src/app/components/page-header/page-header.component.ts
+++ frontend/src/app/components/page-header/page-header.component.ts
@@ -1,11 +1,24 @@
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './page-header.component.html',
   styleUrl: './page-header.component.less',
 })
 export class PageHeaderComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
   @Input() title: string = '';
+  @Input() type: string = ''; // list, detail
+
+  onCreateClick() {
+    this.router.navigate(['create'], { relativeTo: this.route });
+  }
+
+  onEditClick() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
+  }
 }
